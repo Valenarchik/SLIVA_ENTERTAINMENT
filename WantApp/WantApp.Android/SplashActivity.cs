90c5f597e7284a942cc07cf14d4a4323ec0604d4
serge <setu787898@gmail.com>
2022-06-03 12:43:21 +0500

    Theme

---
+++ WantApp/WantApp.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace WantApp.Droid
+{
+    [Activity(MainLauncher = true, Theme = "@style/MyTheme.Splash", NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
